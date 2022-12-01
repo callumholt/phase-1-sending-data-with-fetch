2616b8cca2ae08ad290c73e0ca0f091ae8d9d8a8
callum <callumpholt@gmail.com>
2022-12-01 21:16:25 +1100

    all tests pass

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 // Add your code here
 
-const configurationObject = {
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-        "Accept" : "application/json",
-    },
-    body: JSON.stringify({
-            dogName: "Byron",
-            dogBreed: "poodle"
-        }),
-};
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "content-type": "application/json",
+//         "Accept" : "application/json",
+//     },
+//     body: JSON.stringify({
+//             dogName: "Byron",
+//             dogBreed: "poodle"
+//         }),
+// };
 
-fetch('http://127.0.0.1:3000/dogs', configurationObject)
-.then(function (response) {
-    return response.json();
-})
-.then(function (Object) {
-    console.log(Object);
-})
-.catch(function (error) {
-    alert("Bad Things!!! ARGGGG!");
-    console.log(error.message);
-});
+// fetch('http://127.0.0.1:3000/dogs', configurationObject)
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (Object) {
+//     console.log(Object);
+// })
+// .catch(function (error) {
+//     alert("Bad Things!!! ARGGGG!");
+//     console.log(error.message);
+// });
 
 
-const bodyData = {
-    name: "callum",
-    email: "callumpholt@gmail.com",
-};
 
-const postMethod = {
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-        "Accept" : "application/json",
-    },
-    body: JSON.stringify(bodyData),
-};
 
 
 function submitData(name, email) {
-    fetch("http://localhost:3000/users", postMethod)
+    const bodyData = {
+        name: name,
+        email: email,
+    };
+    
+    const postMethod = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify(bodyData),
+    };
+    return fetch("http://localhost:3000/users", postMethod)
         .then((response) => response.json())
         .then((data) => {
             
@@ -50,7 +51,6 @@ function submitData(name, email) {
             h2.innerHTML = data.id;
             body.appendChild(h2);
             
-        return fetch("http://localhost:3000/users");
         })
 
         .catch((error) => {
@@ -60,7 +60,6 @@ function submitData(name, email) {
             const h2 = document.createElement('h2');
             h2.innerHTML = error;
             body.appendChild(h2);
-        return fetch("http://localhost:3000/users");
     });
 };
 
